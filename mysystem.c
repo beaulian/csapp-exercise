0f983714c8486cc5a779648e76e2c4fd0134607e
shenaishiren <gjw.jesus@qq.com>
2015-08-21 10:24:52 +0800

    第八章(异常控制流)22题

---
+++ mysystem.c
@@ -0,0 +1,39 @@
+#include "csapp.h"
+
+int mysystem(char *command)
+{
+    pid_t pid;
+    int status;
+    if (command == NULL)
+        return -1;
+    if ((pid = Fork()) < 0)
+        return -1;
+
+    else if (pid == 0){
+        char *argv[] = {"sh","-c",command};
+        execve("/bin/sh",argv,NULL);
+        exit(-1); //子进程正常执行则不会执行此语句,因为跳转到第三方程序了
+    }
+
+    while (1){
+        if (waitpid(pid,&status,0) == -1){
+            if (errno != EINTR)
+                return -1;
+        }
+        else{
+            if WIFEXITED(status)
+                return WEXITSTATUS(status);
+            else
+                return status;
+        }
+    }
+
+}
+
+int main(int argc,char *argv[])
+{
+    int status;
+    mysystem(argv[1]);
+    exit(0);
+}
+
