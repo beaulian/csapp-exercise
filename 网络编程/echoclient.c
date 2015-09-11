118d106f9d52a035ff0e097c933c289b07b7e3bd
shenaishiren <gjw.jesus@qq.com>
2015-09-11 16:57:02 +0800

    上传习题

--- 网络编程/echoclient.c
+++ 网络编程/echoclient.c
@@ -15,4 +15,14 @@ int main(int argc, char **argv)
     port = atoi(argv[2]);
 
     clientfd = Open_clientfd(host, port);  /* 返回建立好连接的套接字描述符 */
+    Rio_readinitb(&rio, clientfd); /* 创建一个读缓冲区,并将读缓冲区和clientfd文件描述符联系起来 */
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
+    }
+
+    Close(clientfd);
+    exit(0);
 }
